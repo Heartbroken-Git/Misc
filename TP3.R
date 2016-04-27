80846b43964fa3af511c764229e06a14e3d92b5f
Heartbroken-Git <corentin196@gmail.com>
2016-04-27 18:31:48 +0200

    exercice 3.3 fini

--- TP3.R
+++ TP3.R
@@ -1,8 +1,10 @@
-#Exercice 3.1
+#Exercice 3.1 #A FINIR DE REMPLIR
 
-x <- 1:10
-sample(x)
-sample(x, replace = TRUE)
+x <- 1:10 
+message("x <- 1:10 donne le vecteur [1,2,...,10]")
+sample(x) 
+message("sample(x) tire aléatoirement tous les éléments de x et les place en vecteur")
+sample(x, replace = TRUE) 
 sample(x[x > 8])
 sample(x[x > 9])
 sample(x[x > 10])
@@ -28,16 +30,57 @@ barplot(table(res_dé_pipé)/500, main=titre2)
 urne = c(1,1,1,1,1,0,0,0,0,0,0,0,0,0,0)
 X = 0
 Y = 0
+nbX = c(0,0,0,0,0,0,0,0,0,0,0)
+nbY = c(0,0,0,0,0,0,0,0,0,0,0)
 for (i in 1:500) {
   E1 = sample(urne, 10)
   E2 = sample(urne, 10, replace = TRUE)
-  X = X + sum(E1)
-  Y = Y + sum(E2)
+  nbX[sum(E1)] = nbX[sum(E1)] + 1
+  nbY[sum(E2)] = nbY[sum(E2)] + 1
 }
-freqX = X/500
-freqY = Y/500
-titre1 = "Fréquences obtenues pour \n 500 tirages sans remise"
-titre2 = "Fréquences obtenues pour \n 500 tirages avec remise"
-par(mfrow=c(1,2))
-barplot(X, freqX/500, main=titre1) #Pas fini
-barplot(Y, freqY/500, main=titre2)
+titre1 = "Répartition obtenue pour \n 500 tirages sans remise"
+titre2 = "Répartition obtenue pour \n 500 tirages avec remise"
+par(mfrow=c(2,2))
+barplot(nbX/500, main=titre1, names.arg=labels)
+barplot(nbY/500, main=titre2, names.arg=labels)
+labels=seq(0, 10)
+titre3 = "Répartition théorique des tirages \n de boules blanches sans remise"
+barplot(dhyper(0:10, 5, 10, 10), main=titre3, names.arg=labels)
+titre4 = "Répartition théorique des tirages \n de boules blanches avec remise"
+barplot(dbinom(0:10, 10, 1/3), main=titre4, names.arg=labels)
+
+#Exercice 3.2
+
+message("Sn = X1 + ... + Xn suit une loi binomiale de paramètre n = n et p = p, ici on a fixé n = 5 et p = 0.5")
+
+calculRn = function(n, N, e, p) {
+  sN = rbinom(N, n, p)
+  sWn = sN / n
+  tmp = abs(sWn - p)
+  Rn = sum(tmp > e) / N
+  return(Rn)
+}
+message("Pour la question 2) on a Rn =")
+calculRn(5, 100, 0.1, 0.5)
+
+message("Pour la question 3) pour les différents n on a")
+calculRn(10, 100, 0.1, 0.5)
+calculRn(25, 100, 0.1, 0.5)
+calculRn(50, 100, 0.1, 0.5)
+calculRn(75, 100, 0.1, 0.5)
+calculRn(100, 100, 0.1, 0.5)
+calculRn(150, 100, 0.1, 0.5)
+calculRn(200, 100, 0.1, 0.5)
+calculRn(500, 100, 0.1, 0.5)
+
+message("Pour la question 4) pour e = 0.01 et les différents n on a")
+calculRn(10, 100, 0.01, 0.5)
+calculRn(100, 100, 0.01, 0.5)
+calculRn(200, 100, 0.01, 0.5)
+calculRn(500, 100, 0.01, 0.5)
+calculRn(1000, 100, 0.01, 0.5)
+calculRn(5000, 100, 0.01, 0.5)
+calculRn(10000, 100, 0.01, 0.5)
+calculRn(50000, 100, 0.01, 0.5)
+
+message("On constate que Rn tend vers 0, on en conclut que P(Wn) converge vers p quand n tend vers l'infini")
